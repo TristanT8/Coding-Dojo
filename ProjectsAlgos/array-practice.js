269ea4ad325ee62be3f7bcbc14618a8a440cd665
Tristan Trinh <Tristan.Trinh8@gmail.com>
2023-08-07 15:15:02 -0400

    Array Practice

---
+++ ProjectsAlgos/array-practice.js
@@ -0,0 +1,36 @@
+// var x = 75;
+// if (x > 100) {    
+//     console.log("bigger than 100");
+// }
+// else if (x > 50) {    
+//     console.log("bigger than 50");
+// }
+// else if(x > 25) {
+//     console.log("bigger than 25");
+// }
+// else {    
+//     console.log("small number");
+// }
+// because the first statement is not true, but the second statement is true, it will print "bigger than 50" to our console.  However, it won't even try to check our else if(x > 25) or else statements because it has already found something in the chain that is true.
+
+//T-Diagram Practice
+
+// var num1 = 20;
+// var num2 = 5;
+// if (num1 < num2) {
+//     num2 = num2 * num1;
+// } else {
+//     num1 = num1 / num2;
+//     if (num1 < num2){
+//         num1 = num1 * 2;
+//     } else if (num1 == num2){
+//         num2 = num1 * num2;
+//     } else {
+//         num2 = num2 * 2;
+//     }
+// }
+// console.log(num1);
+// console.log(num2);
+
+
+
