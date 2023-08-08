a9706eb1e0c68345f1441e324d6e6d44296b4602
Tristan Trinh <Tristan.Trinh8@gmail.com>
2023-08-08 18:12:00 -0400

    Arrays practice

--- ProjectsAlgos/arrays1.js
+++ ProjectsAlgos/arrays1.js
@@ -45,4 +45,69 @@
 // console.log("New Array:", array);
 
 
-//Problem 3: 
+// //Problem 3: Given an array, index, and additional value, insert the value into array at given index. You can think of pushFront(arr,val) as equivalent to insertAt(arr,0,val)
+
+// function insertValue(array, index, value) {
+//     //Shift element right
+//     for (let o = array.length; o > index; o--) {
+//         array[o] = array[ o - 1];
+//     }
+//     //Insert new calue at given index
+//     array[index] = value;
+// }
+
+// let array = [0, 5, 10, 20, 25, 30];
+// let insertedIndex = 3; //inserts at this index
+// let valueInserted = 15;
+// insertValue(array, insertedIndex, valueInserted);
+// console.log("New Array:", array);
+
+
+// //Problem 4: Given an array and an index into array, remove and return the array value at that index. Prove the value is removed from the array by printing it.
+
+// function removeAtIndex(array, index) {
+//     if (index < 0 || index >= array.length) {
+//         return undefined;
+//     }
+//     // Get removed value
+//     const removedValue = array[index];
+//     // Shift elements left
+//     for (let i = index; i < array.length - 1; i++) {
+//         array[i] = array[i + 1];
+//     }
+//     // Removes last element
+//     array.length--;
+//     // Returns removed value
+//     return removedValue;
+// }
+
+// let removeArray = [1, 2, 3, 4, 5, 6, 7];
+// let removeIndex = 2;
+// let removedValue = removeAtIndex(removeArray, removeIndex);
+// console.log("Removed value:", removedValue);
+// console.log("New Array:", removeArray);
+
+
+//Problem 5: Given a sorted array, remove duplicate values. Because array elements are already in order, all duplicate values will be grouped together. If you already made the Remove At function, you are welcome to use that! If you solved this using nested loops, for an extra challenge, try to do it without any nested loops!
+
+function removeDuplicates(sortedArray) {
+    if (sortedArray.length <= 1) {
+        return sortedArray;
+    }
+
+    let uniqueIndex = 1; // Index for placing unique values
+
+    for (let i = 1; i < sortedArray.length; i++) {
+        if (sortedArray[i] !== sortedArray[i - 1]) {
+            sortedArray[uniqueIndex] = sortedArray[i];
+            uniqueIndex++;
+        }
+    }
+
+    sortedArray.length = uniqueIndex; // Remove the remaining elements
+    return sortedArray;
+}
+
+let sortedArray = [1, 2, 2, 3, 3, 3, 4, 4, 5, 6, 6, 6, 7];
+let newArray = removeDuplicates(sortedArray);
+console.log("Array with duplicates removed:", newArray);
